d242cda553dca8c8f3b8792726aa867a8a1a3467
nurbaysal <nurbysl__@hotmail.com>
2019-07-18 00:04:42 +0300

    proje

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace YonlendirmeGirisi_HW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.btnGiriş = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(33, 21);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 20);
+            this.txtUser.TabIndex = 0;
+            this.txtUser.TextChanged += new System.EventHandler(this.TxtUser_TextChanged);
+            // 
+            // btnGiriş
+            // 
+            this.btnGiriş.Location = new System.Drawing.Point(42, 60);
+            this.btnGiriş.Name = "btnGiriş";
+            this.btnGiriş.Size = new System.Drawing.Size(75, 23);
+            this.btnGiriş.TabIndex = 1;
+            this.btnGiriş.Text = "Giriş";
+            this.btnGiriş.UseVisualStyleBackColor = true;
+            this.btnGiriş.Click += new System.EventHandler(this.BtnGiriş_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGiriş);
+            this.Controls.Add(this.txtUser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Button btnGiriş;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YonlendirmeGirisi_HW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Eger ilk textbox'a "Admin","Moderator","Yonetici","CEO","Baskan" 
+            //degerleri girilirse "Yönetim Paneline Yönlendiriliyorsunuz..."
+            //"Uye" girilirse "Ana Sayfaya Yonlendiriliyorsunuz...";
+            //Harici bir durumda "Bu Sayfayı Görme Yetkiniz Yok!";
+        }
+
+        private void BtnGiriş_Click(object sender, EventArgs e)
+        {
+            string veri = txtUser.Text.ToLower();
+            switch (veri)
+
+            {
+                case "admin":
+                case "moderator":
+                case "Yonetici":
+                case "ceo":
+                case "baskan":
+                    MessageBox.Show("Yonetim Paneline Yönlendiriliyorsunuz");
+                    break;
+
+                case "uye":
+                    MessageBox.Show("Ana Sayfaya Yonlendiriliyorsunuz");
+                    break;
+
+
+
+
+                default:
+                    MessageBox.Show("Bu Sayfayı Görme Yetkiniz Yok");
+                    break;
+            }
+            txtUser.Clear();
+        }
+
+        private void TxtUser_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
